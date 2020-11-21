25dc0e0b4a01e4f66e0c7bb3b2764bccfcb697cc
Meastrix001 <nroofthooft2012@gmail.com>
2020-11-21 21:40:51 +0100

    relink @work 1

---
+++ static/js/data.js
@@ -0,0 +1,29 @@
+flowerTypes =[{
+  image: "https://uploads-ssl.webflow.com/5f5248796f0e6e75b273d0e2/5f5b688941ef3b4a015b5e77_thomas-ae-295NLwGdrKM-unsplash.jpg",
+  title:'TrouwBoeketten',
+},
+{
+  image: "https://uploads-ssl.webflow.com/5f5248796f0e6e75b273d0e2/5f5b69551db74c59215ae2a1_jez-timms-PDZFmpHJ-aw-unsplash.jpg",
+  title:'Seizoensdecoratie',
+},
+{
+  image:"https://uploads-ssl.webflow.com/5f5248796f0e6e75b273d0e2/5f5b69ab1a25938cabdda049_mayron-oliveira-mibn6LLm9kA-unsplash.jpg",
+  title:'Rouwwerk op maat',
+}]
+
+
+flowerSizes =[{
+  image:"https://uploads-ssl.webflow.com/5f5248796f0e6e75b273d0e2/5f649bb3f9afc00725d6e6b6_Small_kortgebonden_gemengd_2%20(Medium).png",
+  price: '€30/boeket',
+  title:'Boeket small',
+},
+{
+  image:"https://uploads-ssl.webflow.com/5f5248796f0e6e75b273d0e2/5f6f07a131c72f20033b72a7_Medium_Kortgebonden_gemengd%20(Medium).png",
+  price: '€42/boeket',
+  title:'Boeket medium',
+},
+{
+  image:"https://uploads-ssl.webflow.com/5f5248796f0e6e75b273d0e2/5f649d46dc336b61c61e94c1_20200918_132013_Large_langgebonden_gemengd%20(Medium).png",
+  price: '€50/boeket',
+  title:'Boeket large',
+}]
---
+++ static/js/main.js
@@ -0,0 +1,67 @@
+(() => {
+
+    const app = {
+      initialize() {
+        console.log('Website Started')
+        this.indexChecker();
+        this.cacheElements();
+        this.buildUI();
+        this.MenuClickEventlistener()
+      },
+      indexChecker() {
+
+      },
+      cacheElements() {
+        console.log('Elements Cached')
+        this.$test_button = document.querySelector('.navigatemenu')
+        this.$dropDownMenuEvent = document.querySelector('.dropdownmenu')
+        this.$flowerTypesLineUp = document.querySelector('.flower-types_lineup')
+        this.$flowerSizesLineUp = document.querySelector('.flower-Sizes_lineup')
+      },
+      buildUI() {
+        console.log('building UI')
+        this.$flowerTypesLineUp.innerHTML = this.createLineUpForFlowersTypes();
+        this.$flowerSizesLineUp.innerHTML = this.createLineUpForFlowersSizes();
+      },
+
+      MenuClickEventlistener() {
+        this.$test_button.addEventListener('click', (evt) => {
+
+          let check = this.$dropDownMenuEvent
+          if (check.style.visibility === "visible") {
+            check.style.visibility = "hidden";
+          } else {
+            check.style.visibility = "visible";
+          }
+        })
+      },
+      createLineUpForFlowersTypes() {
+        let tempStr = ''
+        flowerTypes.forEach(flower => {
+          tempStr += `
+      <div class="flowerinlineupbackground">
+       <div class="flowerinlineup">
+        <img src="${flower.image}"
+        <p><a href="">${flower.title}</a></p>
+      </div>
+      `
+        });
+        return tempStr;
+      },
+      createLineUpForFlowersSizes() {
+        let tempStr = ''
+        flowerSizes.forEach(flower => {
+          tempStr += `
+      <div class="flowerinlineupbackground">
+       <div class="flowerinlineup">
+        <img src="${flower.image}"
+        <p><a href="">${flower.title}</a>  ${flower.price}</p>
+       </div>
+      </div>
+      `
+        });
+        return tempStr;
+      },
+  }
+  app.initialize();
+})();
