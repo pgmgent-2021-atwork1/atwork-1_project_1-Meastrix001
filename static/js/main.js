3c88119da70daec33c9743bc8a282dfdf8039905
Meastrix001 <nroofthooft2012@gmail.com>
2020-11-25 23:56:44 +0100

    all websites basic layout without finetuning

--- static/js/main.js
+++ static/js/main.js
@@ -1,69 +1,68 @@
 (() => {
 
-    const app = {
-      initialize() {
-        console.log('Website Started')
-        this.indexChecker();
-        this.cacheElements();
-        this.buildUI();
-        this.MenuClickEventlistener()
-      },
-      indexChecker() {
+  const app = {
+    initialize() {
+      console.log('Website Started')
+      this.indexChecker();
+      this.cacheElements();
+      this.buildUI();
+      this.MenuClickEventlistener()
+    },
+    indexChecker() {
 
-      },
-      cacheElements() {
-        console.log('Elements Cached')
-        this.$test_button = document.querySelector('.navigatemenu')
-        this.$dropDownMenuEvent = document.querySelector('.dropdownmenu')
-        this.$flowerTypesLineUp = document.querySelector('.flower-types_lineup')
-        this.$flowerSizesLineUp = document.querySelector('.flower-Sizes_lineup')
-      },
-      buildUI() {
-        console.log('building UI')
-        this.$flowerTypesLineUp.innerHTML = this.createLineUpForFlowersTypes();
-        this.$flowerSizesLineUp.innerHTML = this.createLineUpForFlowersSizes();
-      },
+    },
+    cacheElements() {
+      console.log('Elements Cached')
+      this.$test_button = document.querySelector('.navigatemenu')
+      this.$dropDownMenuEvent = document.querySelector('.dropdownmenu')
+      this.$flowerTypesLineUp = document.querySelector('.flower-types_lineup')
+      this.$flowerSizesLineUp = document.querySelector('.flower-Sizes_lineup')
+    },
+    buildUI() {
+      console.log('building UI')
+      this.$flowerTypesLineUp.innerHTML = this.createLineUpForFlowersTypes();
+      this.$flowerSizesLineUp.innerHTML = this.createLineUpForFlowersSizes();
+    },
 
-      MenuClickEventlistener() {
-        this.$test_button.addEventListener('click', (evt) => {
+    MenuClickEventlistener() {
+      this.$test_button.addEventListener('click', (evt) => {
 
-          let check = this.$dropDownMenuEvent
-          if (check.style.visibility === "visible") {
-            check.style.visibility = "hidden";
-          } else {
-            check.style.visibility = "visible";
-          }
-        })
-      },
-      createLineUpForFlowersTypes() {
-        let tempStr = ''
-        flowerTypes.forEach(flower => {
-          tempStr += `
-      <div class="flowerinlineupbackground">
+        let check = this.$dropDownMenuEvent
+        if (check.style.visibility === "visible") {
+          check.style.visibility = "hidden";
+        } else {
+          check.style.visibility = "visible";
+        }
+      })
+    },
+    createLineUpForFlowersTypes() {
+      let tempStr = ''
+      flowerTypes.forEach(flower => {
+        tempStr += `
        <div class="flowerinlineup">
-        <img src="${flower.image}"
+          <img src="${flower.image}">
         <p><a href="">${flower.title}</a></p>
-      </div>
+        </div>
       `
-        });
-        return tempStr;
-      },
-      createLineUpForFlowersSizes() {
-        let tempStr = ''
-        flowerSizes.forEach(flower => {
-          tempStr += `
-      <div class="flowerinlineupbackground">
+      });
+      return tempStr;
+    },
+    createLineUpForFlowersSizes() {
+      let tempStr = ''
+      flowerSizes.forEach(flower => {
+        tempStr += `
+      
        <div class="flowerinlineup">
         <img src="${flower.image}"
-        <div>
-        <p><a href="">${flower.title}</a>${flower.price}</p>
-       </div>
+         <div>
+          <p><a href="">${flower.title}</a> ${flower.price}</p>
+         </div>
         </div>
-      </div>
+      
       `
-        });
-        return tempStr;
-      },
+      });
+      return tempStr;
+    },
   }
   app.initialize();
 })();
